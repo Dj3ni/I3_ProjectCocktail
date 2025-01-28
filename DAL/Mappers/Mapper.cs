a676dc3fcc791cff1b2141296c0b89391bc857a8
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2025-01-28 16:20:25 +0100

    Partie Dal

---
+++ DAL/Mappers/Mapper.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Mappers
+{
+	//Only class that can be internal (because no use with other projects
+	internal static class Mapper
+	{
+		public static User ToUser(this IDataRecord record)
+		{
+			if (record is null) throw new ArgumentNullException(nameof(record)); //Or if(record is null) return null;
+			return new User()
+			{
+				User_Id = (Guid)record[nameof(User.User_Id)],
+				First_Name = (string)record[nameof(User.First_Name)],
+				Last_Name = (string)record[nameof(User.First_Name)],
+				Email = (string)record[nameof(
+									User.Email)],
+				Password = "********",
+				DisabledAt = (record[nameof(User.DisabledAt)] is DBNull) ? null : (DateTime?)record[nameof(User.DisabledAt)],
+			};
+		}
+	}
+}
---
+++ DAL/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using DAL.Entities;
+using DAL.Mappers;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Services
+{
+	public class UserService
+	{
+		private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WAD24-DemoASP-DB;Integrated Security=True;Connect Timeout=60;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+		//Searching the user in the list
+		public IEnumerable<User> Get()
+		{
+			//Connection to DB
+			using (SqlConnection connection = new SqlConnection(ConnectionString))
+			{
+				//Command
+				using (SqlCommand command = connection.CreateCommand())
+				{
+					//Get with stocked procedure for the Sql command
+					command.CommandText = "SP_User_GetAllActive";
+					command.CommandType = System.Data.CommandType.StoredProcedure;
+					connection.Open();
+					// multiple infos so we use DataReader
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							//We use the mapper we created
+							yield return reader.ToUser();
+						}
+					}
+				}
+			}
+		}
+
+		//Searching the User By the Id
+		public User Get(Guid user_id)
+		{
+			using (SqlConnection connection = new SqlConnection(ConnectionString))
+			{
+				using (SqlCommand command = connection.CreateCommand())
+				{
+					command.CommandText = "SP_User_GetById";
+					command.CommandType = System.Data.CommandType.StoredProcedure;
+					command.Parameters.AddWithValue(nameof(user_id), user_id);
+					connection.Open();
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						if (reader.Read())
+						{
+							return reader.ToUser();
+						}
+						else
+						{
+							throw new ArgumentOutOfRangeException(nameof(user_id));
+						}
+					}
+
+				}
+			}
+		}
+	}
+}
